d014897963f291571b11991f340b2d7212534752
Francois Smith <steamfaffa@gmail.com>
2022-08-02 22:47:56 +0200

    Added Page 2 Skeleton

--- src/components/page-2/page-2-styles.jsx
+++ src/components/page-2/page-2-styles.jsx
@@ -1,5 +1,46 @@
 import styled from "styled-components";
 
 export const PageContainer = styled.div`
+    width: 1000px;
+    position: relative;
+    margin: 20px;
     
+    #page-2-border{
+        position: absolute;
+        width: 1000px;
+        height: auto;
+        top: 0px;
+        left: 0px;
+    }
+
+    #page-2-frames{
+        width: 1000px;
+        height: auto;
+        position: relative;
+        pointer-events: none;
+    }
+
+    #page-2-panel2{
+        position: absolute;
+        width: 483px;
+        height: auto;
+        left: 29px;
+        top: 430px;
+    }
+
+    #page-2-panel4{
+        position: absolute;
+        width: 420px;
+        height: auto;
+        right: 29px;
+        top: 400px;
+    }
+
+    #page-2-panel5{
+        position: absolute;
+        width: 1000px;
+        height: auto;
+        left: 2px;
+        bottom: 2px;
+    }
 `;
--- src/components/page-2/page-2.jsx
+++ src/components/page-2/page-2.jsx
@@ -4,7 +4,11 @@ import { PageContainer } from './page-2-styles'
 function page5() {
     return (
         <PageContainer>
-            
+            <img id="page-2-panel2" src="/media/svgs/page-2/page-2-panel2.svg" alt="Page 2 panel 2"/>
+            <img id="page-2-panel4" src="/media/svgs/page-2/page-2-panel4.svg" alt="Page 2 panel 4"/>
+            <img id="page-2-panel5" src="/media/svgs/page-2/page-2-panel5.svg" alt="Page 2 panel 5"/>
+            <img id="page-2-frames" src="/media/svgs/page-2/page-2-frames.svg" alt="Page 2 frames"/>
+            <img id="page-2-border" src="/media/svgs/page-2/page-2-border.svg" alt="Page 2 border"/>
         </PageContainer>
     )
 }
