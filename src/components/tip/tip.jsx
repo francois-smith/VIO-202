b4c0cc88c1108e3a6233c12646b4d42ee5507b32
Francois Smith <steamfaffa@gmail.com>
2022-08-02 22:47:01 +0200

    Added Tip at Top of Comic

---
+++ src/components/tip/tip.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TipContainer } from './tip-styles'
+
+function page5() {
+    return (
+        <TipContainer>
+            <img src="/media/svgs/Sword.svg" alt="sword" />
+            <span>Hover Over Panels For Best Experience</span>
+            <img src="/media/svgs/Sword.svg" alt="sword" />
+        </TipContainer>
+    )
+}
+
+export default page5;
