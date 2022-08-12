2e6eccc078c2609644c61d519c5d4493998b9798
Francois Smith <steamfaffa@gmail.com>
2022-08-12 23:27:48 +0200

    Added 3 Panels

--- src/components/page-2/page-2.jsx
+++ src/components/page-2/page-2.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { PageContainer } from './page-2-styles'
+import Panel2 from './panels/panel-2/panel-2'
+import Panel3 from './panels/panel-3/panel-3'
+import Panel4 from './panels/panel-4/panel-4'
 
 function page5() {
     return (
         <PageContainer>
-            <img id="page-2-panel2" src="/media/svgs/page-2/page-2-panel2.svg" alt="Page 2 panel 2"/>
-            <img id="page-2-panel4" src="/media/svgs/page-2/page-2-panel4.svg" alt="Page 2 panel 4"/>
+            <Panel2/>  
+            <Panel3/>
+            <Panel4/>  
             <img id="page-2-panel5" src="/media/svgs/page-2/page-2-panel5.svg" alt="Page 2 panel 5"/>
             <img id="page-2-frames" src="/media/svgs/page-2/page-2-frames.svg" alt="Page 2 frames"/>
             <img id="page-2-border" src="/media/svgs/page-2/page-2-border.svg" alt="Page 2 border"/>
