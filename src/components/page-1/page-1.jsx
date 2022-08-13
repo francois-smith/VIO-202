8b128ea89d71d57eefa1f7c5e8ceb70a2671ffba
Francois Smith <steamfaffa@gmail.com>
2022-08-13 17:10:12 +0200

    Swapped Panel 3 With ANimation

--- src/components/page-1/page-1.jsx
+++ src/components/page-1/page-1.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { PageContainer } from './page-1-styles'
 import Panel2 from './panels/panel-2/panel-2'
+import Panel3 from './panels/panel-3/panel-3'
 import Panel4 from './panels/panel-4/panel-4'
 import Panel6 from './panels/panel-6/panel-6'
 
@@ -11,7 +12,7 @@ function page5() {
             <img id="page-1-panel5" src="/media/svgs/page-1/page-1-panel5.svg" alt="Page 1 panel 5"/>
             <img id="page-1-panel1" src="/media/svgs/page-1/page-1-panel1.svg" alt="Page 1 panel 1"/>
             <Panel2/>
-            <img id="page-1-panel3" src="/media/svgs/page-1/page-1-panel3.svg" alt="Page 1 panel 3"/>
+            <Panel3/>
             <Panel4/>
             <Panel6/>
             <img id="page-1-frames" src="/media/svgs/page-1/page-1-frames.svg" alt="Page 1 frames"/>
