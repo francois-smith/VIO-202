df846a606a75a4a22b0ef972372b7d9bc37681c9
Francois Smith <steamfaffa@gmail.com>
2022-08-19 19:42:08 +0200

    Foundation For Page 3

--- src/components/page-3/page-3-styles.jsx
+++ src/components/page-3/page-3-styles.jsx
@@ -1,5 +1,31 @@
 import styled from "styled-components";
 
 export const PageContainer = styled.div`
+    width: 1000px;
+    position: relative;
+    margin: 20px;
     
+    #page-3-frames{
+        position: absolute;
+        width: 1000px;
+        height: auto;
+        top: 0px;
+        left: 0px;
+        pointer-events: none;
+    }
+
+    #page-3-background{
+        width: 999px;
+        height: auto;
+        position: relative;
+        pointer-events: none;
+    }
+
+    #page-3-panel5{
+        position: absolute;
+        width: 947px;
+        height: auto;
+        left: 29px;
+        bottom: 29px;
+    }
 `;
--- src/components/page-3/page-3.jsx
+++ src/components/page-3/page-3.jsx
@@ -4,7 +4,9 @@ import { PageContainer } from './page-3-styles'
 function page5() {
     return (
         <PageContainer>
-            
+            <img id="page-3-background" src="/media/svgs/page-3/page-3-background.svg" alt="Page 3 background"/>
+            <img id="page-3-panel5" src="/media/svgs/page-3/page-3-panel5.svg" alt="Page 3 panel 5"/>
+            <img id="page-3-frames" src="/media/svgs/page-3/page-3-frames.svg" alt="Page 3 frames"/>
         </PageContainer>
     )
 }
