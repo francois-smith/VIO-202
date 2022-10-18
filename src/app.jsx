4a4fcfc438168875ef3282dfd1fceca443928416
Francois Smith <steamfaffa@gmail.com>
2022-10-18 22:16:24 +0200

    Added Button to portfolio

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import Page2 from './components/page-2/page-2'
 import Page3 from './components/page-3/page-3'
 import Page4 from './components/page-4/page-4'
 import Page5 from './components/page-5/page-5'
+import './yeet.css';
 
 function App() {
 	const style={
@@ -14,9 +15,16 @@ function App() {
 		alignItems: 'center',
 	}
 
+	//function to link to another website
+	function openLink(link){
+		window.open(link, "_self");
+	}
+
 	return (
 		<Theme>
 			<div style={style}>
+				{/* greater that symbol */}
+				<button onClick={()=>openLink("https://francois-smith.netlify.app/#ProjectsSection")} className='float-button'><span><i className="fa-solid fa-arrow-left arrow"></i> PORTFOLIO</span></button>
 				<Background/>
 				<Tip/>
 				<Page1/>
